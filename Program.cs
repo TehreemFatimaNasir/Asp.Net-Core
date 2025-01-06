6d19e604e4c0e73984ea1c4202474447f2d2f80d
Tehreem Fatima Nasir <tehreemnasir090@gmail.com>
2025-01-06 18:41:39 +0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,47 +1,11 @@
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllersWithViews();
 var app = builder.Build();
+
 
 //app.MapGet("/", () => "Hello World!");
 
-app.Use(async (context, next) => {
-
-
-    await context.Response.WriteAsync("This is Tehreem Fatima.\n");
-    await next(context);
-
-});
-app.Use(async (context, next) => {
-
-
-    await context.Response.WriteAsync("I'm pursuing diploma in Software Engineering.\n");
-    await next(context);
-
-});
-app.Use(async (context, next) => {
-
-
-    await context.Response.WriteAsync("Also pursuing degree in IT.\n");
-    await next(context);
-
-});
-app.Use(async (context, next) => {
-
-
-    await context.Response.WriteAsync("Aim to continue growing and contributing to the ever-evolving tech industry.\n");
-    await next(context);
-
-});
-app.Run(async (context) => {
-
-    await context.Response.WriteAsync("This is Tehreem Fatima,I'm pursuing diploma in Software Engineering,Also pursuing degree in IT,Aim to continue growing and contributing to the ever-evolving tech industry.\n");
-});
-
-
-
-
-
-
-
+app.MapDefaultControllerRoute();
 
 
 app.Run();
