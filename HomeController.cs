f9204f333466e236e828aae4f1602d2551a17491
Tehreem Fatima Nasir <tehreemnasir090@gmail.com>
2025-01-06 18:45:18 +0500

    Add files via upload

---
+++ HomeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace routing_tempdata.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<string> ENGLISHMONTHS = new List<string>
+            {
+                "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"
+            };
+
+            TempData["ENGLISHMONTHS"] = ENGLISHMONTHS;
+
+            return View();
+        }
+    }
+}
